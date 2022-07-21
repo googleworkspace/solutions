0c763b954d794b0db657758d8e32c4c56b6db5f0
Justin Poehnelt <jpoehnelt@google.com>
2022-07-21 12:26:15 -0600

    chore: add license headers (#219)

--- vacation-calendar/src/Code.js
+++ vacation-calendar/src/Code.js
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 // Set the ID of the team calendar to add events to. You can find the calendar's
 // ID on the settings page.
 var TEAM_CALENDAR_ID = 'ENTER_TEAM_CALENDAR_ID_HERE';
